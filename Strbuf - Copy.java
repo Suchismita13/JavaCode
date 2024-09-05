fc658d20a673427a860fa882317a5d22b0549a95
Suchismita13 <143961796+Suchismita13@users.noreply.github.com>
2024-09-05 15:32:57 +0530

    Add files via upload

---
+++ Strbuf - Copy.java
@@ -0,0 +1,13 @@
+class A
+{
+public static void main(String args[])
+{
+StringBuffer sb=new StringBuffer();
+System.out.println("Initial Capacity:"+sb.capacity());
+System.out.println("String append:"+sb.append("my name is kingkobra"));
+System.out.println("string replace:"+sb.replace(11,15,"deba"));
+System.out.println("updated Capacity:"+sb.capacity());
+System.out.println("String append:"+sb.append("my name is kingkobraaaaaa"));
+System.out.println("updated Capacity:"+sb.capacity());
+}
+}
